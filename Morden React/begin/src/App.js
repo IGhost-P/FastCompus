c607b4cb1fb9055bccbdd63b0092224bef82bea0
IGhost-P <79236624+IGhost-P@users.noreply.github.com>
2021-05-04 13:27:31 +0900

    13 배열에 항목 추가하기

--- Morden React/begin/src/App.js
+++ Morden React/begin/src/App.js
@@ -1,8 +1,21 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '' // input이라는 객체 만듬
+  });
+  const { username, email } = inputs; // 선추출
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value // name키에 있는 값을 value = email or 사용자이름 으로 등록
+    });
+  };
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -18,16 +31,34 @@ function App() {
       username: 'liz',
       email: 'liz@example.com'
     }
-  ];
+  ]);
 
   const nextId = useRef(4);
   const onCreate = () => {
-    // 나중에 구현 할 배열에 항목 추가하는 로직
-    // ...
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers(users.concat(user)); // 컴캣 안에 있는 값을 users안에 합쳐줌 // setUser([...users,user]); //spread 해도 되는데 몇개 없으니 걍함
 
+    setInputs({
+      username: '',
+      email: '' // 초기화
+    });
     nextId.current += 1;
   };
-  return <UserList users={users} />;
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
+  );
 }
 
 export default App;

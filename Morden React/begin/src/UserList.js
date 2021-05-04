989317b0aca3ce08d920c59c65eca010f508c231
IGhost-P <79236624+IGhost-P@users.noreply.github.com>
2021-05-04 14:14:38 +0900

    15 배열에 항목 수정하기

--- Morden React/begin/src/UserList.js
+++ Morden React/begin/src/UserList.js
@@ -1,22 +1,34 @@
 import React from 'react';
 
-function User({ user, onRemove }) {
+function User({ user, onRemove, onToggle }) {
     return (
         <div>
-            <b>{user.username}</b> <span>({user.email})</span>
+            <b
+                style={{
+                    cursor: 'pointer',
+                    color: user.active ? 'green' : 'black'
+                }}
+                onClick={() => onToggle(user.id)}
+            >
+                {user.username}
+            </b>
+      &nbsp;
+            <span>({user.email})</span>
             <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
     );
 }
-{/*파라미터를 넣고 싶기때문에 안에 새로운 함수를 넣는다, 이버튼이 눌리면 ~한 함수를 호출하고, 그리고 그 함수는id라는 파라미터를 가져 온다. 이렇게 안하면 랜더링 되는 순간 함수가 호출 되어버려서 삭제가 되어버림  */ }
-function UserList({ users, onRemove }) {
+
+function UserList({ users, onRemove, onToggle }) {
     return (
         <div>
             {users.map(user => (
                 <User
                     user={user}
                     key={user.id}
-                    onRemove={onRemove} />
+                    onRemove={onRemove}
+                    onToggle={onToggle}
+                />
             ))}
         </div>
     );

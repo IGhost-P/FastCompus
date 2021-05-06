18d4c06944311b1bde40f7f2466f3b59a0413cc9
IGhost-P <79236624+IGhost-P@users.noreply.github.com>
2021-05-06 15:01:28 +0900

    20. useRudcer를 사용하여 상태 엡데이트 로직 분리하기

--- Morden React/begin/src/Counter.js
+++ Morden React/begin/src/Counter.js
@@ -1,22 +1,35 @@
+import React, { useReducer } from 'react';
 
-import React, { useState } from 'react';
-
-function Counter() {
-
-    const [number, setNumber] = useState(0); // 첫번째는 동적 으로 바뀔 값, 첫번째 값에 동작을 주는 함수임
-    const onIncrese = () => {
-        setNumber(prevNumber => prevNumber + 1)
+function reducer(state, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
     }
-    const onDecrese = () => {
-        setNumber(prevNumber => prevNumber + -1) // 
-    }
-    return (
-        <div>
-            <h1> {number} </h1>
-            <button onClick={onIncrese}>+1</button> {/* 리엑트에 이벤트 핸들러 값은, 호출이 아닌 값을 주는것!*/}
-            <button onClick={onDecrese}>-1</button>
-        </div>
-    )
 }
 
-export default Counter
+function Counter() {
+    const [number, dispatch] = useReducer(reducer, 0); // 첫번째 현재의 상태, 두번째 디스패치는 보내다, 액션을 발생시킨다. 라는 뜻/ 리듀서와 기존값을 넣는다
+
+    const onIncrease = () => {
+        dispatch({ type: 'INCREMENT' }); // 상태의 로직이 컴포넌트 밖에 있는것을 확인할수있다.
+    };
+
+    const onDecrease = () => {
+        dispatch({ type: 'DECREMENT' });
+    };
+
+    return (
+        <div>
+            <h1>{number}</h1>
+            <button onClick={onIncrease}>+1</button>
+            <button onClick={onDecrease}>-1</button>
+        </div>
+    );
+}
+
+export default Counter;
+// 타입 이름은 우리 가 설정하고, 그에 따른 액션을 설정한다.

bea8f1fe372954f1b4b3a0f0f843821a715c0a4f
IGhost-P <79236624+IGhost-P@users.noreply.github.com>
2021-05-06 15:02:27 +0900

    20 원래대로

--- Morden React/begin/src/index.js
+++ Morden React/begin/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Counter from './Counter';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>,
   document.getElementById('root') // root라는 아이디에 있는 값에다가 <= App에 넣는다는 말, root가 있는 파일은 Html로 작성 되어 있다 = App에 있는 파일을 root에 넣어서 Html형태로 만들어버린다.
   // + root는 public/index.html에 있다.

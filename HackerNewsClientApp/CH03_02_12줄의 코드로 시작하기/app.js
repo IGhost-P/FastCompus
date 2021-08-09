3a4c0a6eb3d3d5b53fb679e9cdf3273859a23584
IGhos-P <dndb3599@gmail.com>
2021-08-09 17:59:11 +0900

    1강 완료

--- HackerNewsClientApp/CH03_02_12줄의 코드로 시작하기/app.js
+++ HackerNewsClientApp/CH03_02_12줄의 코드로 시작하기/app.js
@@ -1,10 +1,10 @@
-const ajax = new XMLHttpRequest();
-const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const ajax = new XMLHttpRequest(); // DOM API가 HTML의 선택해주는 도구 역할 처럼 햇던것 처럼, 네트워크 너머에 있는 도구를 가져오는 XMLHttpsREquest, 이것을 ajasx라는 저장소에 넣어준다
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'; //원천에서 데이터를 가져온다
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+ajax.open('GET', NEWS_URL, false); // false는 동기적으로 가져온다는 뜻, 선언
+ajax.send(); // 이때 데이터를 가져옴
 
-const newsFeed = JSON.parse(ajax.response);
+const newsFeed = JSON.parse(ajax.response); // ajax.reponsew에 데이터가 들어와있다 , 응답값이 보기 어렵기때문에 JSON값을 객체화 시켜서 만든다
 const ul = document.createElement('ul');
 
 for (let i = 0; i < 10; i++) {
@@ -16,4 +16,3 @@ for (let i = 0; i < 10; i++) {
 }
 
 document.getElementById('root').appendChild(ul);
-ㅔㅁ
--- HackerNewsClientApp/CH03_02_12줄의 코드로 시작하기/dist/app.c328ef1a.js
+++ HackerNewsClientApp/CH03_02_12줄의 코드로 시작하기/dist/app.c328ef1a.js
@@ -118,11 +118,16 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
-var ajax = new XMLHttpRequest();
-var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
-var newsFeed = JSON.parse(ajax.response);
+var ajax = new XMLHttpRequest(); // DOM API가 HTML의 선택해주는 도구 역할 처럼 햇던것 처럼, 네트워크 너머에 있는 도구를 가져오는 XMLHttpsREquest, 이것을 ajasx라는 저장소에 넣어준다
+
+var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'; //원천에서 데이터를 가져온다
+
+ajax.open('GET', NEWS_URL, false); // false는 동기적으로 가져온다는 뜻, 선언
+
+ajax.send(); // 이때 데이터를 가져옴
+
+var newsFeed = JSON.parse(ajax.response); // ajax.reponsew에 데이터가 들어와있다 , 응답값이 보기 어렵기때문에 JSON값을 객체화 시켜서 만든다
+
 var ul = document.createElement('ul');
 
 for (var i = 0; i < 10; i++) {
@@ -132,7 +137,6 @@ for (var i = 0; i < 10; i++) {
 }
 
 document.getElementById('root').appendChild(ul);
-ㅔㅁ;
 },{}],"../../../../../.nvm/versions/node/v14.16.1/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';

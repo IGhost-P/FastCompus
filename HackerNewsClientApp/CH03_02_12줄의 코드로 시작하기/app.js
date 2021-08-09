720b5e8dadc06215c5212daa36f2db2b0fd562f8
IGhos-P <dndb3599@gmail.com>
2021-08-09 17:08:19 +0900

    cd 위치 바꾸고 하니깐됨

--- HackerNewsClientApp/CH03_02_12줄의 코드로 시작하기/app.js
+++ HackerNewsClientApp/CH03_02_12줄의 코드로 시작하기/app.js
@@ -7,7 +7,7 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
 
-for(let i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
   const li = document.createElement('li');
 
   li.innerHTML = newsFeed[i].title;
@@ -16,3 +16,4 @@ for(let i = 0; i < 10; i++) {
 }
 
 document.getElementById('root').appendChild(ul);
+ㅔㅁ

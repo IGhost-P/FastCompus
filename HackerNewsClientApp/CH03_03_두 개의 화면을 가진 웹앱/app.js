3fbca9d50f6c30abb21b2fe856ebe303a82c7f34
IGhos-P <dndb3599@gmail.com>
2021-08-10 12:04:32 +0900

    두개의 화면을 가진 웹앱

--- HackerNewsClientApp/CH03_03_두 개의 화면을 가진 웹앱/app.js
+++ HackerNewsClientApp/CH03_03_두 개의 화면을 가진 웹앱/app.js
@@ -1,8 +1,8 @@
-const container = document.getElementById('root');
+const container = document.getElementById('root'); // 원래 맨 아래 코드가 루트로 계속 반복되었는데 이것을 컨테이너로 묶어서 사용하면, 나중에 root를 안쓰고 변경하더라도 변경하기 쉽디
 const ajax = new XMLHttpRequest();
-const content = document.createElement('div');
+const content = document.createElement('div'); // 태그 만들어놓음
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
-const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'; // id에 값에 따라 주소가 바뀜, 마킹하는 방법은 여러개이지만, @id로 저장
 
 ajax.open('GET', NEWS_URL, false);
 ajax.send();
@@ -10,10 +10,11 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
 
-window.addEventListener('hashchange', function() {
-  const id = location.hash.substr(1);
+window.addEventListener('hashchange', function () { // 해시는 일종의 북마크 인데, name이라고 하는 속성과 같은 해시 이름이 들어오면 그 위치로 바로 스콜링하게 해쥼
+  // 그래서 이 해쉬가 바뀔때 = 해당 타이틀을 눌렀구나로 판단됨
+  const id = location.hash.substr(1); // 로케이션은 주소와 관련된 다영한 정보를 제공해준다, #인 해쉬부분은 필요없으니 substr로 지워준다(해당 인자 이후로만 표현)
 
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+  ajax.open('GET', CONTENT_URL.replace('@id', id), false); // //@id라는 값을 id로 바꿔줌
   ajax.send();
 
   const newsContent = JSON.parse(ajax.response);
@@ -24,16 +25,16 @@ window.addEventListener('hashchange', function() {
   content.appendChild(title);
 });
 
-for(let i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
   const li = document.createElement('li');
-  const a = document.createElement('a');
+  const a = document.createElement('a'); //클릭을 하기 위해선 앵커 태그가 좋음 href를 붙혀줘야 링크처럼 누를수있음
 
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+  a.href = `#${newsFeed[i].id}`; // a에 herf 속성을 넣어줌 ,해쉬에 아이디를 넣어주자
+  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`; // 댓글 표현
 
   li.appendChild(a);
   ul.appendChild(li);
 }
-
 container.appendChild(ul);
-container.appendChild(content);
+container.appendChild(content); //ul 밑으로 추가해줌 
+// 지금은 계속 추가됨
--- HackerNewsClientApp/CH03_04_문자열을 활용한 HTML 다루기/app.js
+++ HackerNewsClientApp/CH03_04_문자열을 활용한 HTML 다루기/app.js
@@ -3,7 +3,7 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-
+// 이코드가 반복되서 나오니깐, 중복되는것은 하나로 만들어준다. 데이터를 담을수 있는 것은 변수, 뱐수 여러개는 객체, 코드 여러개는 함수로
 function getData(url) {
   ajax.open('GET', url, false);
   ajax.send();
@@ -14,7 +14,7 @@ function getData(url) {
 const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
-window.addEventListener('hashchange', function() {
+window.addEventListener('hashchange', function () {
   const id = location.hash.substr(1);
 
   const newsContent = getDat(CONTENT_URL.replace('@id', id))
@@ -25,10 +25,10 @@ window.addEventListener('hashchange', function() {
   content.appendChild(title);
 });
 
-for(let i = 0; i < 10; i++) {
-  const div = document.createElement('div');
-
-  div.innerHTML =  `
+for (let i = 0; i < 10; i++) {
+  const div = document.createElement('div'); // 일단은 div로 넣어주자 나중에 지울거임
+  //DOM API를 이용하면 html구조를 알기 어려워서 , 이렇게 문자열로 넣는 방법이 있다
+  div.innerHTML = ` 
     <li>
       <a href="#${newsFeed[i].id}">
         ${newsFeed[i].title} (${newsFeed[i].comments_count})
@@ -36,7 +36,7 @@ for(let i = 0; i < 10; i++) {
     </li>
   `;
 
-  ul.appendChild(div.firstElementChild);
+  ul.appendChild(div.firstElementChild); // 현재 li라는 태그가 없고 우리는 div로 감싸줬다. div의 첫번째 자식 요소를 가져오면된다. children[0]도 가능
 }
 
 container.appendChild(ul);

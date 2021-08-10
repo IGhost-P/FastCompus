a374875e170a03783b021c31a9bf212b4b0b4149
IGhos-P <dndb3599@gmail.com>
2021-08-10 23:34:48 +0900

    페이징

--- HackerNewsClientApp/CH03_05-2_라우터 화면 처리기 만들기/app.js
+++ HackerNewsClientApp/CH03_05-2_라우터 화면 처리기 만들기/app.js
@@ -11,28 +11,28 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-function newsFeed() {
-  const newsFeed = getData(NEWS_URL);
+function newsFeed() { // 뉴스 피드 분리해서 함수로 만들기
+  const newsFeed = getData(NEWS_URL); //데이터 가져와야하니깐 여기에 넣음
   const newsList = [];
 
   newsList.push('<ul>');
-  
-  for(let i = 0; i < 10; i++) {
+
+  for (let i = 0; i < 10; i++) {
     newsList.push(`
       <li>
-        <a href="#${newsFeed[i].id}">
+        <a href="#${newsFeed[i].id}"> 
           ${newsFeed[i].title} (${newsFeed[i].comments_count})
         </a>
       </li>
     `);
   }
-  
+
   newsList.push('</ul>');
-  
+
   container.innerHTML = newsList.join('');
 }
 
-function newsDetail() {
+function newsDetail() { // 뺴낸뒤에 기명 함수로 바꿔준다
   const id = location.hash.substr(1);
   const newsContent = getData(CONTENT_URL.replace('@id', id))
 
@@ -45,16 +45,16 @@ function newsDetail() {
   `;
 }
 
-function router() {
+function router() { // newFeed를 함수로 만들어 놓았으니, 맨처음에 해당 함수를 호풀할 기능이 필요해서 만듬
   const routePath = location.hash;
 
-  if (routePath === '') {
+  if (routePath === '') { //비어 있으면 첫 진입,location에#만 들어와 있으면 빈 문자열로 인식
     newsFeed();
   } else {
     newsDetail();
   }
 }
 
-window.addEventListener('hashchange', router);
+window.addEventListener('hashchange', router); // 이벤트 핸들러에 묶여 있는 익명 함수이기 때문에 분리 해준다 그리고, 해쉬가 바뀔때마다 페이지를 교체해줄수있게 라우터로 바꿔준다
 
-router();
+router(); // 최초 호출해준다
--- HackerNewsClientApp/CH03_06_페이징 구현하기/app.js
+++ HackerNewsClientApp/CH03_06_페이징 구현하기/app.js
@@ -3,8 +3,8 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-const store = {
-  currentPage: 1,
+const store = { // 전체적으로 관리해야할 변수는 여러개가 있으니, 이렇게 저장소를 만든다.
+  currentPage: 1, // 피드 안에 페이지가 한정되어 있으면, 현재페이지가 몇번째인지 저정 할수 없기 떄문에, 전체에서 변경되어야한다
 };
 
 function getData(url) {
@@ -19,8 +19,8 @@ function newsFeed() {
   const newsList = [];
 
   newsList.push('<ul>');
-  
-  for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+  // 1페이지 일때 맨 처음 i는 0 이여야하기때문에 -1, 그리고 페이지가 넘어 갈때마다 10개의 목록씩 넘어가니깐 * 10을 해준다
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
       <li>
         <a href="#/show/${newsFeed[i].id}">
@@ -29,9 +29,10 @@ function newsFeed() {
       </li>
     `);
   }
-  
+
   newsList.push('</ul>');
-  newsList.push(`
+  // 네비게이션 UI를 만들자
+  newsList.push(` 
     <div>
       <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
       <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
@@ -59,8 +60,9 @@ function router() {
 
   if (routePath === '') {
     newsFeed();
+    // 보여지는 부분과, 페이지부분을 구분했다
   } else if (routePath.indexOf('#/page/') >= 0) {
-    store.currentPage = Number(routePath.substr(7));
+    store.currentPage = Number(routePath.substr(7));//라우터 값에서 페이지 값을 추출함 하지만 문자열이기 때문에, Number로 바꿔줌 (#뒤에 /page/'이부분이')
     newsFeed();
   } else {
     newsDetail()
